d20d2510198a7398d318f66ce46c4cf85ab47384
rahul <rahul@puppetlabs.com>
2012-09-09 15:17:09 -0700

    updates

--- bin/gentestrun.rb
+++ bin/gentestrun.rb
@@ -21,33 +21,40 @@ class TestRun
   def initialize(proj)
     @proj = proj
     FindTests.new('src/' + proj).show.each do |t|
-      next if File.exist? "cov.update/#{proj}/#{t}"
+      next if File.exist? "coverage/update/#{proj}/#{t}"
       y = t.chomp
       puts t
       puts "==================================="
       puts %[cd src/#{proj}; mvn -Dtest=#{y}  -DfailIfNoTests=false emma:emma]
       puts %x[cd src/#{proj}; mvn -Dtest=#{y}  -DfailIfNoTests=false emma:emma]
-      puts %x[mkdir -p cov.update/#{proj}]
+      puts %x[mkdir -p coverage/update/#{proj}]
       emmalst = findemma()
       emmalst.each do |cov|
         case cov
         when /(.*target.site.emma)$/
           emma = $1
           emmaname = emma.gsub("src/#{proj}/",'').gsub(/\//,'%')
-          %x[mkdir -p cov.update/#{proj}/#{t}/]
-          puts %[cp -r #{emma} cov.update/#{proj}/#{t}/#{emmaname}]
+          %x[mkdir -p coverage/update/#{proj}/#{t}/]
+          puts %[cp -r #{emma} coverage/update/#{proj}/#{t}/#{emmaname}]
           puts "==================================="
-          puts %x[cp -r #{emma} cov.update/#{proj}/#{t}/#{emmaname}]
+          puts %x[cp -r #{emma} coverage/update/#{proj}/#{t}/#{emmaname}]
         end
       end
     end
   end
 end
 
-cov = File.open('cov.lst').readlines.each do |x|
+case ARGV[0]
+when /-all/
+cov = File.open('coverage/cov.lst').readlines.each do |x|
   case x
   when /([^ ]+) +([^ ]+)/
     puts $1
   TestRun.new($1.chomp)
   end
+end
+when /-one/
+  TestRun.new(ARGV[1].chomp.split('/')[1])
+else
+  puts "-all | -one src/xxx"
 end
